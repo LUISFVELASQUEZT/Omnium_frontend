9b55db4e6b4c184d177eda005db63763b76f04d9
Luis F. Velásquez T <luisvelasquezt@gmail.com>
2021-12-11 11:36:28 -0300

    To clarifty the message.
    Identifiying where the message comes from

--- src/components/PrivateComponent.jsx
+++ src/components/PrivateComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PrivateComponent = ({ roleList, children }) => {
   const { userData } = useUser();
-  console.log("Hello ", userData.nombre, " your role is ", userData.rol, " your id: /" , userData.identificacion, " and your email is ", userData.correo);
+  console.log(userData.nombre," Hello from PrivateComponent.",  "Your role is ", userData.rol, " your id: /" , userData.identificacion, " and your email is ", userData.correo);
   if (roleList.includes(userData.rol)) {
     return children;
   }
